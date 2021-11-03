ee6be155da53ad191d13bfb2fcf030a3e17b4749
valera mironov <valera4944690@mail.ru>
2021-11-03 16:11:09 +0300

    Example

---
+++ Example/Example006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "валера")
+{
+    Console.WriteLine("OMG, Hi Valera");
+} 
+else
+{
+    Console.WriteLine("hi, friend");
+    Console.WriteLine(username);
+}
